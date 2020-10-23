71b3fb84fdcd45a191d0caffa884fd223c6a0791
Intan Yoshana <intan318@gmail.com>
2020-10-23 18:43:43 +0700

    first commit

---
+++ app/Models/MaterialArea.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MaterialArea extends Model
+{
+    protected $table = "table_material_area";
+    protected $guarded = ['id'];
+
+    // public function materialDetail(){
+    //     return $this->belongsTo('App\Material', 'material_id')
+    // }
+
+    public function
+}
---
+++ database/migrations/2020_10_16_093504_create_table_material.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableMaterial extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_material', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('material_code');
+            $table->string('material_name');
+            $table->string('material_brand');
+            $table->string('material_grade');
+            $table->integer('material_size');
+            $table->string('material_unit');
+            $table->integer('seller_id')->nullable();
+            $table->integer('category_id');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_material');
+    }
+}
